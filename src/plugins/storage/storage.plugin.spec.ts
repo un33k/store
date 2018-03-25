6b81f7c21f380b85fa4bbc02e051c424658048d9
Val Neekman <val@neekware.com>
2018-03-25 12:37:26 -0400

    add test for sessionStorage

--- src/plugins/storage/storage.plugin.spec.ts
+++ src/plugins/storage/storage.plugin.spec.ts
@@ -5,8 +5,9 @@ import { State } from '../../state';
 import { Store } from '../../store';
 import { Action } from '../../action';
 import { NgxsStoragePluginModule } from './storage.module';
+import { NgxsStoragePluginOptions } from './symbols';
 
-describe('NgxsStoragePlugin', () => {
+describe('NgxsStoragePlugin with localStorage', () => {
   let store: Store;
 
   class Increment {}
@@ -53,7 +54,7 @@ describe('NgxsStoragePlugin', () => {
     localStorage.removeItem('@@STATE');
   });
 
-  it('should get initial data from localstorage', () => {
+  it('should get initial data from localStorage', () => {
     localStorage.setItem('@@STATE', JSON.stringify({ counter: { count: 100 } }));
 
     store.select(state => state.counter).subscribe((state: StateModel) => {
@@ -61,7 +62,7 @@ describe('NgxsStoragePlugin', () => {
     });
   });
 
-  it('should save data to localstorage', () => {
+  it('should save data to localStorage', () => {
     store.dispatch(new Increment());
     store.dispatch(new Increment());
     store.dispatch(new Increment());
@@ -72,6 +73,79 @@ describe('NgxsStoragePlugin', () => {
       expect(state.count).toBe(105);
 
       expect(localStorage.getItem('@@STATE')).toBe(JSON.stringify({ counter: { count: 105 } }));
+    });
+  });
+});
+
+describe('NgxsStoragePlugin with sessionStorage', () => {
+  let store: Store;
+
+  class Increment {}
+
+  class Decrement {}
+
+  interface StateModel {
+    count: number;
+  }
+
+  @State<StateModel>({
+    name: 'counter',
+    defaults: { count: 0 }
+  })
+  class MyStore {
+    @Action(Increment)
+    increment({ getState, setState }) {
+      const state = getState();
+      setState({
+        count: Number(state.count) + 1
+      });
+    }
+
+    @Action(Decrement)
+    decrement({ getState, setState }) {
+      const state = getState();
+      setState({
+        count: Number(state.count) - 1
+      });
+    }
+  }
+
+  beforeEach(() => {
+    sessionStorage.setItem('@@STATE', JSON.stringify({ counter: { count: 100 } }));
+
+    TestBed.configureTestingModule({
+      imports: [
+        NgxsStoragePluginModule.forRoot(<NgxsStoragePluginOptions>{ storage: 'sessionStorage' }),
+        NgxsModule.forRoot([MyStore])
+      ]
+    });
+
+    store = TestBed.get(Store);
+  });
+
+  afterEach(() => {
+    sessionStorage.removeItem('@@STATE');
+  });
+
+  it('should get initial data from sessionStorage', () => {
+    sessionStorage.setItem('@@STATE', JSON.stringify({ counter: { count: 100 } }));
+
+    store.select(state => state.counter).subscribe((state: StateModel) => {
+      expect(state.count).toBe(100);
+    });
+  });
+
+  it('should save data to sessionStorage', () => {
+    store.dispatch(new Increment());
+    store.dispatch(new Increment());
+    store.dispatch(new Increment());
+    store.dispatch(new Increment());
+    store.dispatch(new Increment());
+
+    store.select(state => state.counter).subscribe((state: StateModel) => {
+      expect(state.count).toBe(105);
+
+      expect(sessionStorage.getItem('@@STATE')).toBe(JSON.stringify({ counter: { count: 105 } }));
     });
   });
 });
