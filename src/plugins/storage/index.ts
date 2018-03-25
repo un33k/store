d363e01bfaca98a034a846061c7809e8e6c541b8
Val Neekman <val@neekware.com>
2018-03-25 12:30:09 -0400

    rename localstorage to storage

--- src/plugins/storage/index.ts
+++ src/plugins/storage/index.ts
@@ -1,2 +1,2 @@
-export { NgxsStoragePluginModule } from './localstorage.module';
-export { NgxsStoragePlugin } from './localstorage.plugin';
+export { NgxsStoragePluginModule } from './storage.module';
+export { NgxsStoragePlugin } from './storage.plugin';
